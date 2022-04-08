0e371d1c14df5a6761fe72a0dfb33185844c36a9
Gabrielle Skelton <gabrielleskeltonn@gmail.com>
2022-04-08 11:30:13 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+/* This is a multi-line comment. 
+   I can use it to write a brief description of my program 
+   that will not be displayed on my console: 
+
+   My code will print a message after I run it.  */
+
+        public class HelloWorld {
+            public static void main(String[] args) {
+                 // This is a single-line comment. 
+                 System.out.println("Hello, World");
+            }
+        }
